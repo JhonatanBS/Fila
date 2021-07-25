4da19ecfab80b5e85b8874182243846039357ddf
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-07-25 15:52:46 -0300

    Foram criadas structs para guardar os dados e as funções protótipas para a implementação, junto da função Principal (Main).

---
+++ FilaPilha/pilhaFila.h
@@ -0,0 +1,34 @@
+struct pilha{
+    int valorPilha;
+    struct pilha* proxPilha;
+};
+
+struct fila{
+    int valorFila;
+    struct fila* proxFila; 
+};
+
+typedef struct pilha Pilha;
+typedef struct fila Fila;
+
+//Funções e Métodos
+Fila* insere(Fila* p,int valor);
+Pilha* push(Pilha* topo,int x);
+void imprimeFila(Fila* p);
+void imprimePilha(Pilha* topo);
+Fila* removeFila(Fila* p);
+Pilha* removePilha(Pilha* topo);
+int tamanhoFila(Fila* p);
+int tamanhoPilha(Pilha* topo);
+int capturaFila(Fila* p);
+int capturaPilha(Pilha* p);
+Fila* liberaFila(Fila* p);
+Pilha* liberaPilha(Fila* topo);
+
+//Funções para o Menu
+void criarLinhaSuperior(int tamx);
+void criarLinhaRodape(int tamx);
+void CriarMenu(int tamx,char str[]);
+void menuPrincipal();
+void menuFila();
+void menuPilha();
---
+++ FilaPilha/pilhaFilaExecucao.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilhaFila.c"
+
+int main (){
+
+    return 1;
+}
